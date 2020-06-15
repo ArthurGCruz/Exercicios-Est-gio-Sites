33a84dcfd70e9bc7db26fd91c3b12a3cefffa5c5
Arthur Cruz <arthurarte29@gmail.com>
2020-06-15 16:24:17 -0300

    Exercicio

---
+++ exercicios americanas/ame2.rb
@@ -0,0 +1,18 @@
+require "selenium-webdriver"
+require "rspec"
+
+describe "Redirecionamento" do
+
+    it "Tela Celular/Smartphone" do
+    driver = Selenium::WebDriver.for :chrome
+
+    driver.navigate.to "https://www.americanas.com.br/" 
+    sleep 5
+    element = driver.find_element(:xpath,"/html/body/div[3]/header/div[2]/div[4]/div/div/ul/li[3]/a")
+    element.click
+
+    expect(driver.title).to eq "Celulares e Smartphones em Promoção nas americanas"
+    sleep 5
+    driver.quit
+     end
+end
